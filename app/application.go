4baa6f0873578d16538ce22acc59ac0af3077ce1
auliayaya <aulia.illahi.ai@gmail.com>
2022-07-18 23:41:55 +0700

    dao structure

---
+++ app/application.go
@@ -0,0 +1,16 @@
+package app
+
+import "github.com/gin-gonic/gin"
+
+var (
+	router = gin.Default()
+)
+
+func startApplication() {
+	mapUrls()
+	router.Run(":8080")
+}
+
+func Start() {
+	startApplication()
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/auliayaya/go-book_users-api/app"
+)
+
+func main() {
+	app.Start()
+}
